daaecf26b9b44fab4069509f99c9d3125cb26aee
LaLe1993 <63840101+LaLe1993@users.noreply.github.com>
2020-06-10 17:07:25 +0200

    React

---
+++ MODUL 4/day 8/m4-d8-react-recap-netflix-master/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router , Route} from 'react-router-dom';
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Route path='/' exact component={Home} />
+        <Route path='/register' exact component={Register} />
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
