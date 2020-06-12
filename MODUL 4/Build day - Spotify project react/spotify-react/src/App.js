2527566fc86a9ffd03887661d24160a0d3b99a3a
LaLe1993 <63840101+LaLe1993@users.noreply.github.com>
2020-06-12 12:50:24 +0200

    Update

--- MODUL 4/Build day - Spotify project react/spotify-react/src/App.js
+++ MODUL 4/Build day - Spotify project react/spotify-react/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer';
-import NavBar1 from './components/NavBar1';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar1 />
+      <NavBar />
       <Footer />
     </div>
   );
