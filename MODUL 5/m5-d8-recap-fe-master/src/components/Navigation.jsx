d506dca039c35358aaac7276a91c65cc2f8a4a17
LaLe1993 <63840101+LaLe1993@users.noreply.github.com>
2020-07-01 17:10:14 +0200

    Navbar dark

--- MODUL 5/m5-d8-recap-fe-master/src/components/Navigation.jsx
+++ MODUL 5/m5-d8-recap-fe-master/src/components/Navigation.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom"
 class Navigation extends Component {
     render() {
         return (
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="dark" expand="lg" className='navbar-dark'>
             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
